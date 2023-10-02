fcebd849dc1e0129f47fe1d5043f82dce8152e24
yasingunay <av.yasingunay@gmail.com>
2023-10-02 21:22:45 +0200

    pagination added

--- network/views.py
+++ network/views.py
@@ -5,15 +5,20 @@ from django.shortcuts import render,get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from django.core.paginator import Paginator
 
 from django.contrib import messages
 from .models import User, Post, Following
 
 
 def index(request):
-    posts = Post.objects.all()
+    post_list = Post.objects.all().order_by('-timestamp')
+    paginator = Paginator(post_list, 2)  # Show2  contacts per page.
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     return render(request, "network/index.html",{
-        "posts": posts
+        "page_obj": page_obj
     })
 
 
@@ -108,8 +113,16 @@ def following_view(request):
     # Extract the users being followed from the Following objects
     users_followed = [following.user_followed for following in user_following]
     # Filter posts by users being followed
-    posts = Post.objects.filter(user__in=users_followed)
+    post_list = Post.objects.filter(user__in=users_followed).order_by('-timestamp')
+    paginator = Paginator(post_list, 2)  # Show2  contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     return render(request, "network/index.html",{
-        "posts": posts
+        "page_obj": page_obj
     })
 
+
+
+
+
+    
